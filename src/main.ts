977df89e5ecbb931b9bb8da5085bd079d2279770
Neila Miranda <161175405+BellaTheAlien@users.noreply.github.com>
2025-10-15 03:43:53 +0000

    Gave a title to the webpage and added a canvas (step 1 complete) - gave the page "Sticker SketchBook" title - added a 256x256 canvas - added style to it, with a balck liner, round eges, and a background shadow

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,16 @@
-import exampleIconUrl from "./noun-paperclip-7598668-00449F.png";
+//import exampleIconUrl from "./noun-paperclip-7598668-00449F.png";
 import "./style.css";
 
 document.body.innerHTML = `
-  <p>Example image asset: <img src="${exampleIconUrl}" class="icon" /></p>
 `;
+
+const h1Element: HTMLHeadingElement = document.createElement("h1");
+
+h1Element.textContent = "Sticker SketchBook";
+document.body.append(h1Element);
+
+const canvas = document.createElement("canvas");
+canvas.id = "canvas";
+canvas.width = 256;
+canvas.height = 256;
+document.body.append(canvas);
