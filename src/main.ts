71a6a0fc3ef82221bd5a4da51a02d11ac12ada81
Neila Miranda <161175405+BellaTheAlien@users.noreply.github.com>
2025-10-16 02:51:50 +0000

    section wk 3 projects -Noah help with the start of step 3, NOT complete
    Co-authored-by: NoahBaron05 <NoahBaron05@users.noreply.github.com>

--- src/main.ts
+++ src/main.ts
@@ -17,15 +17,24 @@ canvas.height = 256;
 document.body.append(canvas);
 
 //context for the canvas
-const ctx = canvas.getContext("2d");
+const ctx = canvas.getContext("2d") as CanvasRenderingContext2D;
 //reading the mouse position
 const cursor = { active: false, x: 0, y: 0 };
+
+//array to store the strokes
+const dispatch = new EventTarget();
+
+interface Stroke {
+  points: { x: number; y: number }[];
+}
+const newStroke = [];
 
 //event listeners for the mouse movements
 canvas.addEventListener("mousedown", (e) => {
   cursor.active = true;
   cursor.x = e.offsetX;
   cursor.y = e.offsetY;
+  //newStroke.push({ points: [{ x: cursor.x, y: cursor.y }] });
 });
 
 canvas.addEventListener("mouseup", () => {
@@ -33,7 +42,7 @@ canvas.addEventListener("mouseup", () => {
 });
 
 canvas.addEventListener("mousemove", (e) => {
-  if (cursor.active && ctx) {
+  if (cursor.active) {
     ctx.beginPath();
     ctx.moveTo(cursor.x, cursor.y);
     ctx.lineTo(e.offsetX, e.offsetY);
@@ -41,6 +50,10 @@ canvas.addEventListener("mousemove", (e) => {
     cursor.x = e.offsetX;
     cursor.y = e.offsetY;
   }
+  /*
+    newStroke.push({ points: [{ x: cursor.x, y: cursor.y }] });
+    dispatch.dispatchEvent(new CustomEvent("strokeAdded", { detail: newStroke }));
+    */
 });
 
 //making the clear button
@@ -51,7 +64,16 @@ document.body.append(clearButton);
 
 //the event listener for the clear button
 clearButton.addEventListener("click", () => {
-  if (ctx) {
-    ctx.clearRect(0, 0, canvas.width, canvas.height);
-  }
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
 });
+
+/*
+function redraw() {
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+  newStroke.forEach((strokes) => {
+    ctx.beginPath();
+    ctx.moveTo(strokes.points[0].x, strokes.points[0].y);
+  });
+}
+dispatch.addEventListener("strokeAdded", redraw);
+*/
